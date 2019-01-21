8bce8d77f8f25d458001f42a50b2605e301ee01e
active-object-6703 <nerajno@gmail.com>
2019-01-21 15:47:19 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,21 +1,25 @@
 def welcome
-  # code #welcome here
+  puts"Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+ puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts"Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
 def end_game
@@ -40,5 +44,5 @@ end
 
 def runner
   # code runner here
+  welcome
 end
-    
